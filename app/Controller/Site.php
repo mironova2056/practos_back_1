e2828357f6dfbfc4b43e243cd28e69099be63df0
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2025-05-01 19:23:16 +0700

    views

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -1,14 +1,16 @@
 <?php
 namespace Controller;
-
+use Couchbase\View;
+use Src\View;
 class Site
 {
     public function index(): void{
-        echo 'working index';
+        $view = new View();
+        return $view->render('site.hello', ['message' => 'index working']);
     }
     public function hello(): void
     {
-        echo 'working hello';
+        return new View('site.hello', ['message' => 'hello working']);
     }
 
 }
