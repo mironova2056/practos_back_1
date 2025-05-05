fd0c88b4153c89cdfb0fe4601a34d1a03cd8ea67
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2025-05-05 21:55:58 +0700

    auth function

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -6,6 +6,7 @@ use Model\Post;
 use Src\View;
 use Src\Request;
 use Model\User;
+use Src\Auth\Auth;
 
 class Site
 {
@@ -21,9 +22,27 @@ class Site
     }
     public function signup(Request $request): string
     {
-        if ($request->method==='POST' && User::create($request->all())){
-            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/go');
         }
         return new View('site.signup');
+    }
+    public function login(Request $request): string
+    {
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 }
